1696ea4a3030da7fdb2d5490e840b786a2936b5c
china-temps <152966347+china-temps@users.noreply.github.com>
2023-12-20 11:25:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# Test!
+# Test! Add a feed source ...
 echo 'src-git kenzo https://github.com/66o99/openwrt-packages' feeds.conf.default
 echo 'src-git small https://github.com/66o99/small' feeds.conf.default
 
