9d21f891890c2391b4adfcc9bf8750cbf7beae3d
china-temps <152966347+china-temps@users.noreply.github.com>
2023-12-21 11:40:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 默认开启 wifi
+sed -i 's_disabled=1_disabled=0_'  package/kernel/mac80211/files/lib/wifi/mac80211.sh
